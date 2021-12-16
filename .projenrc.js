229f0beb864831a8e80161342458a04b4aeee1d2
yehudi-beseye <yehudi.cheng@beseye.com>
2021-12-16 03:33:56 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,7 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'lamdba-bash-demo',
+});
+project.synth();
